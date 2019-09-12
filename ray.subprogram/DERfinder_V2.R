924ee8f1bfe53b85b4e2cbafb8c7ca8cd6dc5d13
Rainer K. Sachs, Professor Emeritus <sachs@math.berkeley.edu>
2019-09-11 22:45:08 -0700

    Add files via upload
    Use these 2 files together with the input files in the main folder within which this a level 1 subfolder to check properties of our DERs

---
+++ ray.subprogram/DERfinder_V2.R
@@ -0,0 +1,82 @@
+#Newer DERs to try out
+source('Datatable.R')
+source('DERfinder_function_V2.R')
+
+#Our IDERs (Individual Dose Effect Relations). Applicable to the 1-ion components of a mixed simulated GCR beam 
+#Modifying NTE1 and NTE2 by insisting they be twice continuously differentiable and monotonic increasing. Double check NTE1, NTE2, Our model
+library("forecast")
+
+#================================ DER MODELS ==================================#
+
+#PW to RKS: Please stick to these two names for now: NEW1, NEW2. 
+#           Don't change the function names yet, but only what is in the function.
+# Available parameter names are kap0, sig0, eta0 and eta1
+
+# func_NEW2 = function(d, L,Z.b, kap0, sig0, eta0){ # using 5th power to improve a bit
+#      P=(1-exp(-Z.b/kap0))^5
+#      sigma=sig0*P+(eta0*L/6.24)*(1-P)
+#      return =(BG_CA+ (sigma*6.24*(d/L)*(1 - exp(-1024*d/L))*(1-exp(-1024*d/L))))
+# }
+# This calibrates if starting value of eta0 is chosen as 100 instead of 10
+# It has summary,AIC, and CV by fold that is slightly better than the one right below.
+# The CV fold improvement holds for almost every (or every?) one of the 17 folds
+# ========= Available parameter names are kap0, sig0, eta0 and eta1   ====== #
+func_NEW2 = function(d, L,Z.b, kap0, sig0, eta0){
+  P=(1-exp(-Z.b/kap0))^2
+  sigma=sig0*P+(eta0*L/6.24)*(1-P)
+  return =(BG_CA+ (sigma*6.24*(d/L)*(1 - exp(-1024*d/L))*(1-exp(-1024*d/L))))
+} # kap0 in numerator and starting value 1/2000 gives the same result
+func_NEW2 = function(d, L,Z.b, kap0, sig0, eta0){
+     P=(1-exp(-Z.b/kap0))^4
+     sigma=sig0*P+(eta0*L/6.24)*(1-P)
+     return =(BG_CA+ (sigma*6.24*(d/L)*(1 - exp(-1024*d/L))*(1-exp(-1024*d/L))))
+    } # this is much better than above as regards summary, AIC, CV by fold, IEA behavior, and ribbon behavior.
+
+###################### New candidate model 1: NEW1 
+
+# func_2para=function(d, L, eta0, sig0){
+#   return(BG_CA + sig0*6.24*d/L*(1-exp(-1024*d/L)) + eta0*(1-exp(-10^5*d)))
+# } # the simple old NTE-also model
+# func_NEW2 = function(d, L,Z.b, kap0, sig0, eta0){
+#   P=(1-exp(-Z.b/kap0))^4
+#   sigma=sig0*P+(eta0*L/6.24)*(1-P)
+#   return =(BG_CA+ (sigma*6.24*(d/L)*(1 - exp(-1024*d/L))*(1-exp(-1024*d/L))))
+# } # to be canniblized for NEW1
+#kap0 604.38518  ; sig0   5.04177  ;    eta0   0.03860; 
+# ========= Available parameter names are kap0, sig0, eta0 and eta1   ====== #
+# func_NEW1=function(d, L, Z.b, kap0,sig0, eta1){
+#     P=(1-exp(-Z.b/kap0))^4
+#     sigma=sig0*P+(.0386*L/6.24)*(1-P)
+#     return =(BG_CA+ (sigma*6.24*(d/L)*(1 - exp(-1024*d/L))*(1-exp(-1024*d/L)) +
+#                eta1*(1-exp(-10^5*d))))
+# }
+func_NEW1=function(d, L, Z.b, kap0,sig0,eta0, eta1){
+  P=(1-exp(-Z.b/kap0))^4
+  sigma=sig0*P+(eta0*L/6.24)*(1-P)
+  return =(BG_CA+ (sigma*6.24*(d/L)*(1 - exp(-1024*d/L))*(1-exp(-1024*d/L)) +
+                     eta1*(1-exp(-10^5*d))))
+}
+#================================ MODEL CALIBRATION ==================================#
+# nlsLM requires a starting value for the parameters to be calibrated. 
+result = calibration_HZE("NEW1", StartValues = list(kap0= 600,sig0 =5,eta0 =.04, eta1 = .045))
+parameters_NEW1 = result[[1]]
+sig_NEW1 = result[[2]]
+model_NEW1 = result[[3]]
+print(summary(model_NEW1))
+summary(model_NEW1, correlation=TRUE)
+result = calibration_HZE("NEW2", StartValues = list(kap0 = 2000, sig0 = 100, eta0=.5))
+# result = calibration_HZE("NEW2", StartValues = list(kap0 = 2000, sig0 = 10, eta0=.5))
+parameters_NEW2 = result[[1]]
+sig_NEW2 = result[[2]]
+model_NEW2 = result[[3]]
+
+print(summary(model_NEW2) )
+# 
+#================================ MODEL COMPARISON ==================================#
+HZE_models = c("NEW1", "NEW2","4para","3para","2para","2paraTE")
+print("AIC, BIC, CV_function(NEW1, by_fold = T), and CV_function(NEW2, by_fold = T)")
+print(sapply(HZE_models, AIC_function))
+print(sapply(HZE_models, BIC_function))
+#sapply(HZE_models, CV_function) #Please look at CV results by_fold. Removing O350 makes nls not not converge, but the other ions work well.
+print(CV_function("NEW1", by_fold = T))
+print(CV_function("NEW2", by_fold = T))
---
+++ ray.subprogram/DERfinder_function_V2.R
@@ -0,0 +1,166 @@
+#4para 
+func_4para = function(d, L, Z.b, eta0, eta1, sig0, kap0) {
+  P = (1-exp(-Z.b/kap0))^2
+  sig = sig0*P + 0.041/6.24*L*(1-P) # 0.041 +- 0.0051 comes from 16Cacao
+  eta = eta0*L*exp(-eta1*L)
+  return(BG_CA + sig*6.24*d/L*(1-exp(-1024*d/L)) + eta*(1-exp(-10^5*d)))
+} 
+
+#3para 
+func_3para = function(d, L, eta0, eta1, sig0){
+  eta = eta0*L*exp(-eta1*L)
+  return(BG_CA + 6.24*sig0*d/L*(1 - exp(-1024*d/L)) + eta*(1-exp(-10^5*d)))
+}
+
+#2para 
+func_2para=function(d, L, eta0, sig0){
+  return(BG_CA + sig0*6.24*d/L*(1-exp(-1024*d/L)) + eta0*(1-exp(-10^5*d)))
+}
+
+#2paraTE 
+func_2paraTE = function(d, L, Z.b, kap0, sig0){
+  P = (1 - exp(-Z.b/kap0))^2
+  sigma = sig0*P + (0.041 * L/6.24)*(1-P)
+  return(BG_CA + (sigma*6.24*(d/L))*(1 - exp(-1024*(d/L))))
+}
+
+#Swift Light Ion 2 parameter 
+func_sli_exp2 <- function(a, C, d) {
+  return(BG_CA + C*(exp(a*d)-1))
+}
+
+#Swift Light Ion 3 paramter 
+func_sli_exp3 <- function(a, b, C, d) {
+  return(BG_CA + C*(exp(a*d+b*d^2)-1))
+}
+
+start_values = list( 
+  #Potential parameters for HZE
+  eta0 = 0.001,
+  eta1 = 0.01,
+  sig0 = 100,
+  kap0 = 500, 
+  #Potential parameters for SwiftLight
+  a = -0.5, b = -10, C = -1
+)
+
+calibration_HZE = function(model_name, data = HZE_data, StartValues = start_values, return_model = F){
+  #A function that returns the calibrated model for HZE and only takes in a model name (as string)
+  #StartValues is a list with starting guesses assigned to each parameter.
+  
+  func_name = paste("func_", model_name, sep ="") #This is the name of the function corresponding to the model
+  func = eval(parse(text = func_name)) #This is the actual function
+  arguments = formalArgs(func)
+  start = StartValues[names(StartValues) %in% arguments] #Getting the start values for the corresponding parameters
+  formula = paste("CA ~ ", func_name, "(", paste(arguments, collapse = ", "), ")", sep = "")
+  model = nlsLM(formula = formula, data = data, start = start, weights = (1/(data$error)^2))
+  coefs = coef(model)
+  parameters_df = data.frame(value = as.numeric(coefs), model = model_name, parameter = names(coefs))
+  if(return_model){
+    return(model)
+  }
+  else{
+    return(list(
+      parameters = parameters_df,
+      sigma = vcov(model),
+      model = model))
+  }
+}
+
+# calibration_SLI = function(model_type, npara, data = SwiftLight_data, return_model = F){
+#   if (grepl("linear", tolower(model_type))){ #If this is a linear model, then we do not need to use nls anymore.
+#     if (npara == 1){
+#       model = lm(I(CA - BG_CA) ~ 0 + d, data = data, weights = 1/error^2)
+#     }
+#     else if (npara == 2){
+#       model = lm(I(CA - BG_CA) ~ d, data = data, weights = 1/error^2)
+#     }
+#   }
+#   else if (grepl("exp", tolower(model_type))){
+#     
+#   }
+# }
+
+#Calibration for 2-parameter model
+result = calibration_HZE("2para")
+parameters_2para = result[[1]]
+sig_2para = result[[2]]
+model_2para = result[[3]]
+summary(model_2para) 
+
+#Calibration for 3-parameter model
+result = calibration_HZE("3para")
+parameters_3para = result[[1]]
+sig_3para = result[[2]]
+model_3para = result[[3]]
+
+#Calibration for 4-parameter model
+result = calibration_HZE("4para")
+parameters_4para = result[[1]]
+sig_4para= result[[2]]
+model_4para = result[[3]]
+
+#Calibration for 2-parameter TE Only Model
+result = calibration_HZE("2paraTE")
+parameters_2paraTE = result[[1]]
+sig_2paraTE = result[[2]]
+model_2paraTE = result[[3]]
+summary(model_2paraTE)
+
+#Calibration for SLI Linear 1 parameter
+model_sli_linear1 <- lm(I(CA - BG_CA) ~ 0 + d, data = SwiftLight_data, weights = (1/(SwiftLight_data$error)^2))
+ccoef <- coef(model_sli_linear1)
+parameters_sli_lin1 <- data.frame(value = as.numeric(ccoef), model = "sli_lin1", parameter = names(ccoef))
+sig_lin1 <- vcov(model_sli_linear1)
+
+#Calibration for SLI Linear 2 parameter
+model_sli_linear2 <- lm(I(CA - BG_CA) ~ 0 + d + I(d^2), data = SwiftLight_data, weights = (1/(SwiftLight_data$error)^2))
+ccoef <- coef(model_sli_linear2)
+parameters_sli_lin2 <- data.frame(value = as.numeric(ccoef), model = "sli_lin2", parameter = names(ccoef))
+sig_lin2 <- vcov(model_sli_linear2)
+
+#================================ MODEL COMPARISON ==================================#
+
+AIC_function = function(model_name) {
+  model = calibration_HZE(model_name = model_name, return_model = T)
+  RS = resid(model)**2 %>% as.numeric()
+  n = length(RS)
+  k = model %>% coef %>% length
+  WRSS = sum((1/HZE_data$error^2)*RS)
+  return(n + n*log(2*pi) + n*log(WRSS/n) + 2*(k+1))
+}
+BIC_function = function(model_name) {
+  model = calibration_HZE(model_name = model_name, return_model = T)
+  RS = resid(model)**2 %>% as.numeric()
+  n = length(RS)
+  k = model %>% coef %>% length
+  WRSS = sum((1/HZE_data$error^2)*RS)
+  return(n + n*log(2*pi) + n*log(WRSS/n) + log(n)*(k+1))
+}
+
+CV_function = function(model_name, data = HZE_data, by = "label", by_fold = F){
+  #Leave-one-ion-out Cross Validation. The train set is one type of ion. Outputs a weighted sum of weighted MSE
+  #by_fold = T gives Weighted MSE by each fold (each ion) with the test size (number of rows in the data belonging to that ion)
+  if (tolower(by) %in% c("label", "labels")){
+    folds = unique(data$label)
+    by = "label"
+  }
+  else if(tolower(by) %in% c("ion","ions")){
+    folds = unique(data$ion)
+    by = "ion"
+  }
+  WMSE = numeric(length(folds))
+  n = numeric(length(folds))
+  for (i in 1:length(folds)){
+    train_index = which(folds[i] == data[, by])
+    train = data[-train_index,]
+    test = data[train_index,]
+    n[i] = nrow(test)
+    model = calibration_HZE(model_name, data = train, return_model = T)
+    prediction = predict(model, test)
+    RS = (test$CA - prediction)**2
+    WMSE[i] = mean((1/test$error^2)*RS)
+  }
+  if(by_fold){return(data.frame(ion = folds, WMSE = WMSE, n = n))}
+  else{return(sum(n*WMSE/nrow(data)))}
+}
